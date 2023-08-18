3dabd53d5bf27ebae9a00213de99c652a3fbef32
Thomas Ulrich <thomasulrich1402@gmail.com>
2023-08-18 15:03:07 -0400

    button press now added class to tag
    when button is pressed the tag adds/removes the class 'on' as needed

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use strict'
 
+//add event for when button gets pressed
 const relayGroup = document.querySelectorAll('.relay');
 relayGroup.forEach(item => item.addEventListener("click", event => {
-    console.log('click');
+    if(item.classList.contains("on")){
+        item.classList.remove("on");
+    }else{
+        item.classList.add("on");
+    }
 }));
