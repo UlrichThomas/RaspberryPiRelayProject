65cd126a4160cb26bc1c3659e4b0fce27e880891
Thomas Ulrich <thomasulrich1402@gmail.com>
2023-08-21 15:24:23 -0400

    Script now adds to db.json
    implemented functionality to add id values to db.json array

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-'use strict'
+//'use strict'
+
+const url = 'http://localhost:3000/activeRelays'
 
 async function readJsonFile(){
     try{
-        const response = await fetch('activeRelays.json');
+        const response = await fetch(url);
         const data = await response.json();
         return data;
     } catch (error) {
@@ -13,10 +15,10 @@ async function readJsonFile(){
 
 async function writeJsonFile(data){
     try{
-        const response = await fetch('activeRelays.json', {
-            method: 'PUT',
+        const response = await fetch(url, {
+            method: 'POST',
             headers: {
-                'Content-Type': 'applications/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         });
@@ -33,22 +35,15 @@ async function writeJsonFile(data){
 //add event for when button gets pressed
 const relayGroup = document.querySelectorAll('.relay');
 relayGroup.forEach(item => item.addEventListener("click", event => {
-    
-    if(item.classList.contains("on")){
-        item.classList.remove("on");
-        readJsonFile().then(jsonArray => {
-            console.log('Orginal JSON array: ', jsonArray);
-            const id = item.id;
-            jsonArray = jsonArray.filter(item => item !== id);
-            writeJsonFile(jsonArray);
-        })
-    }else{
-        item.classList.add("on");
-        readJsonFile().then(jsonArray => {
-            console.log('Orginal JSON array: ', jsonArray);
-            const id = item.id;
+    event.preventDefault();
+    let id = item.id;
+    readJsonFile().then(jsonArray => {
+        const index = jsonArray.indexOf(id);
+        if(index !== -1){
+            jsonArray.splice(index, 1);
+        } else {
             jsonArray.push(id);
-            writeJsonFile(jsonArray);
-        })
-    }
+        }
+        writeJsonFile([id]);
+    });
 }));
